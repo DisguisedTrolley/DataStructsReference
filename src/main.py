b0f52bda8e8c7a0d3974d5f8e8e8babcec172f2c
Samarth Bhat <samarthrbhat52@gmail.com>
2025-05-14 13:12:53 +0530

    Backup commit

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from array_queue import ArrayQueue
+from typing import List
+
+from binary_trees import BSTree
 
 
 def main():
-    queue = ArrayQueue(0)
-    queue.add(1)
+    return
+
+
+def bin_tree_combinations(n: int) -> List[List[int]]:
+    test_arr = [1, 2, 3, 4, 5, 6]
+
+    for i in range(len(test_arr)):
+        head_node = test_arr[i]
+        new_arr = test_arr[0:i] + test_arr[i + 1 :]
+
+
+def create_tree(head_node: int, array: List[int]) -> BSTree:
+    return
 
 
 if __name__ == "__main__":
